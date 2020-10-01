3acc2d4ec1002a30884a71c637fa644e9b660e82
Magnus-Charles <71400268+Magnus-Charles@users.noreply.github.com>
2020-10-01 12:28:50 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 basic.showIcon(IconNames.Yes)
 basic.forever(function () {
-    robotbit.Servo(robotbit.Servos.S1, 0)
+    robotbit.Servo(robotbit.Servos.S2, 0)
     basic.pause(1000)
-    robotbit.Servo(robotbit.Servos.S1, 180)
+    robotbit.Servo(robotbit.Servos.S2, 180)
     basic.pause(1000)
 })
